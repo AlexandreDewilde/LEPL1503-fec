31d269a1d4245f6d271aafa92455bf32806c4768
allEyezOnCode <alexandredewilde2001@gmail.com>
2022-03-13 17:49:05 +0100

    prepare for error handeling

--- src/system.c
+++ src/system.c
@@ -2,6 +2,9 @@
 
 uint8_t *gf_256_full_add_vector(uint8_t *symbol_1, uint8_t *symbol_2, uint32_t symbol_size) {
     uint8_t *add_vector = (uint8_t *) malloc(symbol_size);
+    if (add_vector == NULL) {
+        return NULL;
+    }
     for (uint32_t i = 0; i < symbol_size; i++) {
         add_vector[i] = symbol_1[i] ^ symbol_2[i];
     }
@@ -11,6 +14,9 @@ uint8_t *gf_256_full_add_vector(uint8_t *symbol_1, uint8_t *symbol_2, uint32_t s
 
 uint8_t *gf_256_mul_vector(uint8_t *symbol, uint8_t coef, uint32_t symbol_size) {
     uint8_t *mul_vector = (uint8_t *) malloc(symbol_size);
+    if (mul_vector == NULL) {
+        return NULL;
+    }
     for (uint32_t i = 0; i < symbol_size; i++) {
         mul_vector[i] = gf256_mul_table[coef][symbol[i]];
     }
@@ -20,7 +26,9 @@ uint8_t *gf_256_mul_vector(uint8_t *symbol, uint8_t coef, uint32_t symbol_size) 
 
 uint8_t *gf_256_inv_vector(uint8_t *symbol, uint8_t coef, uint32_t symbol_size) {
     uint8_t *inv_vector = (uint8_t *) malloc(symbol_size);
-    
+    if (inv_vector == NULL) {
+        return NULL;
+    }
     for (uint32_t i = 0; i < symbol_size; i++) {
         inv_vector[i] = gf256_mul_table[symbol[i]][gf256_inv_table[coef]];
     }
@@ -34,9 +42,29 @@ void gf_256_gaussian_elimination(uint8_t **A, uint8_t **b, uint32_t symbol_size,
         for (uint32_t i = k + 1; i < system_size; i++) {
             uint8_t factor = gf256_mul_table[A[i][k]][gf256_inv_table[A[k][k]]];
             uint8_t *sub_line = gf_256_mul_vector(A[k], factor, system_size);
+            if (sub_line == NULL) {
+                exit(EXIT_FAILURE);
+            }
+
             uint8_t *b_sub_line = gf_256_mul_vector(b[k], factor, symbol_size);
+
+            if (b_sub_line == NULL) {
+                // TODO 
+                // how we deal with error?
+            }
+
             A[i] = gf_256_full_add_vector(A[i], sub_line, system_size);
+
+            if (A[i] == NULL) {
+                // TODO 
+            }
+
             b[i] = gf_256_full_add_vector(b[i], b_sub_line, symbol_size);
+
+            if (b[i] == NULL) {
+                // TODO 
+            }
+
             free(sub_line);
             free(b_sub_line);
         }
@@ -49,11 +77,23 @@ void gf_256_gaussian_elimination(uint8_t **A, uint8_t **b, uint32_t symbol_size,
         do {
             uint8_t factor = gf256_mul_table[A[j][i]][gf256_inv_table[A[i][i]]];
             uint8_t *b_sub_line = gf_256_mul_vector(b[i], factor, symbol_size);
+
+            if (b_sub_line == NULL) {
+                // TODO 
+            }
+
             b[j] = gf_256_full_add_vector(b[j], b_sub_line, symbol_size);
             free(b_sub_line);
+
+            if (b[j] == NULL) {
+                // TODO 
+            }
         } while (j--);
 
         b[i] = gf_256_inv_vector(b[i], A[i][i], symbol_size);
+        if (b[i] == NULL) {
+            // TODO 
+        }
     }
 }
 
@@ -69,9 +109,37 @@ uint8_t **gen_coefs(uint32_t seed, uint32_t nss, uint32_t nrs) {
     uint8_t **coefs = malloc(nrs);
     for (uint32_t i = 0; i < nrs; i++) {
         coefs[i] = malloc(nss);
+        if (coefs[i] == NULL) {
+            return NULL;
+        }
         for (uint32_t j = 0; j < nss; j++) {
             coefs[i][j] = (uint8_t) tinymt32_generate_uint32(&prng);
         }
     }
     return coefs;
+}
+
+int main()
+{
+    uint8_t A[3][3] = { {1,2,3}, {5,5,6}, {11,8,9}};
+    uint8_t *A_copy[3] = {A[0], A[1], A[2]};
+    uint8_t b[3][3] = {{0,1,2},{1,1,1},{2,2,2}};
+    uint8_t *b_copy[3] = {b[0], b[1], b[2]};
+
+    gf_256_gaussian_elimination(A_copy, b_copy, 3, 3);
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+            printf("%d ", A_copy[i][j]);
+        printf("\n");
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+            printf("%d ", b_copy[i][j]);
+        printf("\n");
+    }
+    
 }
