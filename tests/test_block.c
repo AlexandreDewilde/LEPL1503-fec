32d71a6a7355831d608ca56d00241a8629fb4ed3
Patrick Noah <Patrick.ebonanoah@student.uclouvain.be>
2022-04-06 20:36:08 +0200

    first approach of test i will do it on saturday

---
+++ tests/test_block.c
@@ -0,0 +1,15 @@
+
+
+
+
+void test_get_file_info();
+void test_prepare_block();
+void test_make_block();
+void test_free_blocks();
+void test_find_lost_words();
+void test_make_linear_system();
+void test_process_block();
+void test_write_block();
+void write_last_block();
+void test_blocks_to_message_t();
+void test_parse_file();
