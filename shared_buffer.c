0cd15a25904aef8d216789c63bc0969c2e42c86f
Patrick Noah <Patrick.ebonanoah@student.uclouvain.be>
2022-04-19 09:20:51 +0200

    Creation of the file for the shared Buffer

---
+++ shared_buffer.c
@@ -0,0 +1,13 @@
+#include <errno.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <stdio.h> 
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+u_int8_t **create_shared_buffer(){
+    //TO DO
+    return NULL;
+
+}
