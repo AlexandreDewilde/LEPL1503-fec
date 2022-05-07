589a8471f6f19841731c9dd4d1d1b825cfee3aad
allEyezOnCode <alexandredewilde2001@gmail.com>
2022-05-07 21:06:57 +0200

    update test time

--- test_time.py
+++ test_time.py
@@ -3,15 +3,20 @@ import time
 import matplotlib.pyplot as plt
 
 tests = []
+n = 100
 
-for i in range(1, 5):
-    start = time.time()
+for i in range(1, 9):
+    
     cmd = f"./fec Binary_doc_test -f tests.txt -n {i}"
-    for _ in range(1000):
+    total = 0
+    for _ in range(n):
+        start = time.time()
         os.system(cmd)
-    end = time.time()
-    tests.append((end - start) / 100)
+        total += time.time() - start
+    tests.append(total / n)
 
-#TODO ajouter légende
-plt.plot(tests)
+plt.title("Temps d'exécution du programme en fonction du nombre N de threads")
+plt.xlabel("Nombre N + 2 threads")
+plt.ylabel("Temps en ms mis par le programme python pour une exécution")
+plt.plot(range(1, len(tests)+1), tests)
 plt.show()
