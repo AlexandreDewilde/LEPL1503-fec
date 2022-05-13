619f8fdb22a1c456f32c88f6ff126bc69c2bed3d
Patrick Noah <Patrick.ebonanoah@student.uclouvain.be>
2022-05-13 23:51:46 +0200

    Remove unsuable test

--- tests/test_block.c
+++ tests/test_block.c
@@ -66,14 +66,6 @@ void test_find_lost_words(){
     free(block1);
 }
 
-void test_prepare_block();
-void test_make_block();
-void test_free_blocks();
-void test_make_linear_system();
-void test_process_block();
-void test_write_block();
-void write_last_block();
-void test_blocks_to_message_t();
-void test_parse_file();
+
 
 
